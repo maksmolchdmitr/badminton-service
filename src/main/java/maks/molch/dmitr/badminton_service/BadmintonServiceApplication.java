c621db510d3f17229e036a2c0274730a0c21685a
maksmolch <maksmolchdmitr@gmail.com>
2025-12-20 00:49:23 +0300

    init

---
+++ src/main/java/maks/molch/dmitr/badminton_service/BadmintonServiceApplication.java
@@ -0,0 +1,13 @@
+package maks.molch.dmitr.badminton_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadmintonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadmintonServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maks/molch/dmitr/badminton_service/BadmintonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package maks.molch.dmitr.badminton_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadmintonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
