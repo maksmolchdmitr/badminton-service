4944a08cdc2bf0205b996f1a4762005a9484b4f6
maksmolch <maksmolchdmitr@gmail.com>
2026-01-01 18:19:46 +0300

    submit

--- src/main/java/maks/molch/dmitr/badminton_service/config/CorsConfig.java
+++ src/main/java/maks/molch/dmitr/badminton_service/config/CorsConfig.java
@@ -1,10 +1,16 @@
 package maks.molch.dmitr.badminton_service.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@ConditionalOnProperty(
+        prefix = "app.cors",
+        name = "enabled",
+        havingValue = "true"
+)
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
