75520ff4c4497ecd9a6b68fab16a92e8ae726107
maksmolch <maksmolchdmitr@gmail.com>
2026-01-06 16:30:55 +0300

    submit

---
+++ src/main/java/maks/molch/dmitr/badminton_service/controller/UsersApiDelegateImpl.java
@@ -0,0 +1,27 @@
+package maks.molch.dmitr.badminton_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import maks.molch.dmitr.badminton_service.generated.api.UsersApiDelegate;
+import maks.molch.dmitr.badminton_service.generated.model.User;
+import maks.molch.dmitr.badminton_service.service.time.TimeService;
+import maks.molch.dmitr.badminton_service.util.TimeUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.time.ZoneId;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class UsersApiDelegateImpl implements UsersApiDelegate {
+    private final TimeService timeService;
+
+    @Override
+    public ResponseEntity<User> getCurrentUser() {
+        return ResponseEntity.ok(new User(
+                UUID.randomUUID().toString(),
+                134341314L,
+                timeService.now(ZoneId.systemDefault())
+        ));
+    }
+}
